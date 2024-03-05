fe7cca9bc57d6cf154f4c38dc51d07e33f294a0a
JulenCuadra <52250190+JulenCuadra@users.noreply.github.com>
2024-03-05 12:15:43 +0100

    Add files via upload

---
+++ EventOperator/Python_Scripts/Event_Manager.py
@@ -0,0 +1,97 @@
+from kubernetes import client, config, watch
+import tipos
+import os
+import random
+import string
+
+# Parámetros de la configuración del objeto
+namespace = "default"
+client_batch = client.BatchV1Api() #Creamos el cliente para interactuar con la API de Kubernetes, en concreto con los recursos de tipo batch, como el Job.
+client_resources = client.CoreV1Api()
+
+action_list = {}#Valorar volcar esto a un ConfigMap, que el ¿Controlador de componentes rellene ese configmap? En el caso de que no se use nuestra extensión deberían declarar ese listado de acciones al ConfigMap de alguna forma distinta.
+action_list = {'dummy-action', 'not-dummy-action'}
+def update_action_list():
+    #TODO leería el ConfigMap y volcaría el listado de acciones disponibles a este gestor.
+    #TODO cuándo debería hacerlo?
+    pass
+
+def cache_available_actions():
+    for action in action_list:
+        #TODO ver https://github.com/senthilrch/kube-fledged que ya se encarga de este problema.
+        #Todo otra opción es crear un daemonset que mande el contenedor a sleep y entonces ya está cacheado.
+        pass
+
+def pasar_a_ejecucion(nombre):
+    job=tipos.job_pasar_a_ejecucion(nombre)
+    cliente_batch=client.BatchV1Api()
+    cliente_batch.create_namespaced_job(namespace, job)
+
+def action_is_available(action_name):
+    checked = False
+    #Comprobar que la acción está disponible de entre las acciones conocidas.
+    #TODO al usar el configmap el listado de acciones va a estar en un fichero en /config/
+    if action_name in action_list:
+        checked = True
+    return checked
+
+def handle_event(event, action): #Tanto el evento como la accion tienen el formato de Kubernetes.
+    client_batch = client.BatchV1Api()#TODO Modificar porque no tiene sentido que se cree varias veces
+    if action_is_available(action['name']):
+        job = tipos.job_event_handler(event.metadata.name + action['name'] + '-'.join(random.choices(string.ascii_lowercase + string.digits, k=5)))
+        job['spec']['template']['spec']['containers'][0]['image'] = action['image']
+        job['spec']['template']['spec']['containers'][0]['imagePullPolicy'] = 'Always'
+        job['spec']['template']['spec']['containers'][0]['env'][0][
+            'customization'] = action['customization'] #OJO es un array habría que pasarlo como tal.
+        client_batch.create_namespaced_job(namespace, job)
+
+
+def check_event_action_relationships(source,event):
+    client_customresources = client.CustomObjectsApi()#TODO modificar esto porque no tiene sentido clearlo varias veces.
+    #TODO, las proximas sentencias serán distintas para los recursos externos a nuestra extensión.
+    resource = client_customresources.get_namespaced_custom_object('ehu.gcis.org','v1alpha1','default','components','dummy-component')#todo obtener la parte final del source para nombre de componente
+    event_action_list = resource['spec']['events']
+    for listed_event in event_action_list:
+        if event.metadata.name in listed_event['name']: #Si el nombre del evento está entre el listado de eventos del recurso.
+            for action in listed_event['actions']: #Por cada acción asociada a ese evento.
+                handle_event(event, action)
+            pass
+
+def watcher_events(client):
+    watcher = watch.Watch()
+    for event in watcher.stream(client.list_event_for_all_namespaces):
+        #TODO evaluar si merece la pena añadir a los eventos un label para hacer esta discriminación o si dejarlo discriminado en la action. Probado con label.
+        #TODO el event manager debería acceder al recurso componente y ver la relación entre evento y sus acciones. Creada una función para checkear esto.
+        #TODO ver como podría el event manager saber que acciones hay que cachear. Ver evento de recurso creado, obtener ese recurso y ver si tiene eventos, leer sus acciones y cachearlas.
+        #TODO ver como el event manager podría conocer el listado de acciones disponibles
+        objeto = event['object']
+        tipo = event['type']
+        print("Nuevo evento: ", "Hora del evento: ", objeto.first_timestamp, "Tipo de evento: ", tipo, "Motivo:", objeto.reason, "Mensaje:", objeto.message)
+
+
+        if objeto.metadata.labels:
+            if 'cause' in objeto.metadata.labels:
+                if objeto.metadata.labels['cause'] == 'ehu.gcis': #Si el evento ha sido lanzado por un recurso nuestro.
+                    check_event_action_relationships(objeto.source.component,objeto)
+                elif objeto.metadata.labels['cause'] == 'external': # Cause existe pero no es de ehu.gcis, se presupone que no utilizan nuestra extensión.
+                    check_event_action_relationships(objeto.source.component,objeto) #TODO, el triggerer deberá generar un
+                else: #Hay un label cause pero no es de nuestra extension event driven
+                    pass
+
+#TODO el gestor debería tener dos hilos, uno de ellos para el watcher de eventos y otro para cachear las acciones y actualizar su listado.
+#Si utilizamos kube fledged no haría falta  este segundo hilo.
+def action_manager():
+    update_action_list()
+    cache_available_actions()
+def gestor():
+    # TODO Forma para utilizar desde Pycharm
+    #path = os.path.abspath(os.path.dirname(__file__))
+    #path = path.replace('EventOperator\\Python_Scripts', "")
+    #config.load_kube_config(os.path.join(os.path.abspath(path), "k3s.yaml"))  # Cargamos la configuracion del cluster
+    #config.load_kube_config("/etc/rancher/k3s/k3s.yaml")
+    config.load_incluster_config() #Forma para obtener la configuración desde dentro del cluster.
+    watcher_events(client.CoreV1Api())
+
+
+if __name__ == '__main__':
+	gestor()
---
+++ EventOperator/Python_Scripts/Event_Triggerer.py
@@ -0,0 +1,114 @@
+import random
+import string
+from json import loads
+
+from kubernetes import client, config
+import tipos
+#import paho.mqtt.client as mqtt
+import os
+import sys
+import time
+import kafka
+
+# Parámetros de la configuración del objeto
+namespace = "default"
+
+
+MQTT_HOST = "mosquitto"
+USER = 'admin'
+PASSWORD = 'mosquittoGCIS'
+IP_BROKER = "192.168.1.1"
+APPEVENTS_TOPIC='ApplicationEvents'
+
+# def subscriber_mqtt():
+#
+#     def on_connect(client, userdata, flags, rc):
+#         if rc == 0:
+#             print("Connected to MQTT broker.")
+#             global connected
+#             connected = True
+#         else:
+#             print("Connection failed: ") + str(rc)
+#         sys.stdout.flush()
+#
+#     def on_message(client, userdata, message):
+#         print("Datos del topico: " + str(message.topic))
+#         print(message.payload)
+#         raise_application_event2k8s_event(message)
+#
+#     clientMqtt = mqtt.Client()
+#     clientMqtt.username_pw_set(USER, password=PASSWORD)
+#     clientMqtt.connect(IP_BROKER, 31883)
+#     clientMqtt.loop_start()
+#     clientMqtt.subscribe(APPEVENTS_TOPIC, qos=0)
+#     clientMqtt.on_connect = on_connect
+#     clientMqtt.on_message = on_message
+#     while True:
+#         pass
+
+def subscriber_kafka():
+    IP_server = "mi-cluster-mensajeria-kafka-bootstrap.kafka-ns"
+    consumer = kafka.KafkaConsumer('ApplicationEvents', bootstrap_servers=[IP_server + ':9092'],value_deserializer=lambda x: loads(x.decode('utf-8')), client_id='appevents-consumer')
+    while True:
+        for msg in consumer:
+            print('He recibido algo:')
+            print(msg)
+            raise_application_event2k8s_event(msg)
+
+def event_parser(message):
+    event = tipos.evento('Este es un evento simulado para poner en ejecucion una aplicacion.', 'AplicacionDesplegada',
+                         'app' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)), 'Test')
+    # event['action'] = 'DESPLEGAR'
+
+    aux={
+        'source' : {
+        'component': message['CONTEXT']['source'], #TODO modificar source porque no acepta '/' compatibilizar eso con formato uri
+    },
+        'metadata':{
+            'name' : message['CONTEXT']['source']+'-'+message['CONTEXT']['id']+'-event',
+            'annotations':{
+                'data' : string(message['DATA'])#Todo modificar data porque en las anotaciones solo acepta strings.
+            },
+            'labels': {
+                'cause': 'ehu.gcis'
+            }
+        },
+    }
+
+    event['action'] = message['CONTEXT']['type']
+    event['event_time'] = message['CONTEXT']['time']
+    event['message'] = message['CONTEXT']['type']
+    event['reason'] = 'CloudEvent '+message['CONTEXT']['specversion'] +' caused by ' + message['CONTEXT']['subject']
+    event['reporting_component'] = message['CONTEXT']['source']
+    event['type']=message['CONTEXT']['type']
+    event.update(aux)
+
+    return event
+
+def raise_application_event2k8s_event(message): #Debería discriminar de alguna forma el mensaje que le llega, que imagino será diferente desde MQTT o Kafka.
+    k8s_client = client.CoreV1Api()
+
+    #Ojo esto está programado para los mensajes que le lleguen por MQTT, para los mensajes de Kafka habrá que hacerlo distinto.
+    #decodedMessage = loads(message.decode('utf-8'))
+    decodedMessage = message.value
+
+    k8s_event = event_parser(decodedMessage)
+    print(k8s_event)
+    k8s_client.create_namespaced_event(namespace, k8s_event)  # Creamos el Kubernetes Event
+    #TODO gestionar como un try para que no colisione si el evento ya existe (a veces repite).
+
+def application_events_watcher():
+    #subscriber_mqtt()
+    subscriber_kafka()
+
+def triggerer():
+    # TODO Forma para utilizar desde Pycharm
+    #path = os.path.abspath(os.path.dirname(__file__))
+    #path = path.replace('EventOperator\\Python_Scripts', "")
+    #config.load_kube_config(os.path.join(os.path.abspath(path), "k3s.yaml"))  # Cargamos la configuracion del cluster
+    # config.load_kube_config("/etc/rancher/k3s/k3s.yaml")  # Cargamos la configuracion del cluster
+    config.load_incluster_config()
+    application_events_watcher()
+
+if __name__ == '__main__':
+	triggerer()
